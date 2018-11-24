44022564188d7f9d05afa965b60fba1bfca38109
TRAN VIET THANH <saoziha1997@gmail.com>
2018-11-24 10:25:52 +0700

    Adding simple swipe

--- Classes/SceneNewGame.cpp
+++ Classes/SceneNewGame.cpp
@@ -7,8 +7,8 @@
 USING_NS_CC;
 Snake *snake;
 float xMovement = 0;
-int framesCount; 
-float newPosX=0;
+int framesCount;
+float newPosX = 0;
 int score;
 Label *label;
 
@@ -42,14 +42,14 @@ bool SceneNewGame::init()
 
 	auto menuImage = Menu::create(closeItem1, nullptr);
 	menuImage->setPosition(Vec2::ZERO);
-	addChild(menuImage);	
+	addChild(menuImage);
 
 	//Score
 	calculateScore();
 
 	snake = new Snake(this);
 	snake->Init();
-	
+
 	/*KeyBoard listener*/
 	/*auto listener = EventListenerKeyboard::create();
 	listener->onKeyPressed = CC_CALLBACK_2(SceneNewGame::onKeyPressed, this);
@@ -69,15 +69,15 @@ bool SceneNewGame::init()
 	int typeRock;
 	for (int i = 0; i < MAX_ROCK; i++)
 	{
-		typeRock = random(1,5);
+		typeRock = random(1, 5);
 		Rock* rock = new Rock(this, typeRock);
 		rock->setType(typeRock);
 		rock->setAlive(false);
 		mRocks.push_back(rock);
 	}
-	
+
 	scheduleUpdate();
-	return true;	
+	return true;
 }
 
 void SceneNewGame::update(float delta)
@@ -85,29 +85,30 @@ void SceneNewGame::update(float delta)
 	framesCount++;
 
 	snake->Update();
-	
+
 	newPosX = snake->GetPosistion().x + STEP *xMovement;
+
 	if (newPosX >= MAX_MOVE_LEFT_W && newPosX <= MAX_MOVE_RIGHT_W)
 	{
 		snake->setPosition(Vec2(newPosX, snake->GetPosistion().y));
-	}		
+	}
 
 	//generating rock
 	if (framesCount % ROCK_GENERATING_STEP == 0)
 	{
 		GenerateRock();
-	}	
-	
+	}
+
 	//update rock
 	for (int i = 0; i < mRocks.size(); i++)
 	{
 		Rock *r = mRocks.at(i);
 		if (r->isAlive())
 		{
-			r->Update();			
+			r->Update();
 		}
 	}
-		
+
 	//UPDATE SCORE
 	score++;
 	if (framesCount % FRAME_CALCULATE_SCORE == 0)
@@ -116,7 +117,6 @@ void SceneNewGame::update(float delta)
 	}
 
 	//newPosX = snake->GetPosistion().x + STEP;
-	
 
 	//UPDATE COLISSION
 	//snake->Colission(mRocks);
@@ -140,10 +140,10 @@ void SceneNewGame::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d
 {
 	switch (keyCode)
 	{
-	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:	
+	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
 		xMovement--;
 		break;
-	case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:	
+	case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
 		xMovement++;
 		break;
 	}
@@ -153,10 +153,10 @@ void SceneNewGame::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2
 {
 	switch (keyCode)
 	{
-	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:	
+	case EventKeyboard::KeyCode::KEY_LEFT_ARROW:
 		xMovement++;
 		break;
-	case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:	
+	case EventKeyboard::KeyCode::KEY_RIGHT_ARROW:
 		xMovement--;
 		break;
 	}
@@ -164,62 +164,37 @@ void SceneNewGame::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2
 
 bool SceneNewGame::onTouchBegan(cocos2d::Touch * touch, cocos2d::Event * event)
 {
-	//currentTouchPos0 = touch->getLocation().x;	
 	initialTouchPos0 = touch->getLocation().x;
-
-	if (initialTouchPos0 < 240)
-	{
-		CCLOG("Touched LEFT");
-		xMovement--;
-	}
-	if (initialTouchPos0 > 240)
-	{
-		CCLOG("Touched RIGHT");
-		xMovement++;
-	}
-	
-	/*isTouchDown = true;*/
-
 	return true;
 }
 
 void SceneNewGame::onTouchMoved(cocos2d::Touch * touch, cocos2d::Event * event)
 {
 	isTouchDown = true;
-
+	currentTouchPos0 = touch->getLocation().x;
 
 	// swipe button
-	/*if (true == isTouchDown)
+	if (true == isTouchDown)
 	{
-		if (initialTouchPos0 - currentTouchPos0  >= 0)
+		if (initialTouchPos0 - currentTouchPos0 >= 0)
 		{
-			CCLOG("SWIPED LEFT");			
+			CCLOG("SWIPED LEFT");
+			xMovement--;
 			isTouchDown = false;
 		}
 		else if (initialTouchPos0 - currentTouchPos0 <= 0)
 		{
-			CCLOG("SWIPED RIGHT");			
+			CCLOG("SWIPED RIGHT");
+			xMovement++;
 			isTouchDown = false;
 		}
 	}
-
-	if (initialTouchPos0 - currentTouchPos0 >= MAX_MOVE_LEFT_W && initialTouchPos0 - currentTouchPos0 <= MAX_MOVE_RIGHT_W)
-	{
-		snake->setPosition(Vec2(STEP, snake->GetPosistion().y));
-	}*/
+	log("Movement: %d", xMovement);
 }
 
 void SceneNewGame::onTouchEnded(cocos2d::Touch * touch, cocos2d::Event * event)
 {
-	initialTouchPos0 = touch->getLocation().x;
-	if (initialTouchPos0 < 240)
-	{
-		xMovement++;
-	}
-	if (initialTouchPos0 > 240)
-	{
-		xMovement--;
-	}
+	xMovement = 0;
 	isTouchDown = false;
 }
 
@@ -234,5 +209,5 @@ void SceneNewGame::calculateScore()
 	label->setColor(Color3B::RED);
 	label->setAlignment(cocos2d::TextHAlignment::CENTER);
 	label->setPosition(Vec2(SCORE_X, SCORE_Y));
-	addChild(label);	
+	addChild(label);
 }
